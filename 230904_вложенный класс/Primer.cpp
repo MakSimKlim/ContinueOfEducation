e63aba30cfdcfa42c14b2d9ab7d84dbacd7485ca
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-09-04 21:50:15 +0500

    Add files via upload

--- 230904_вложенный класс/Primer.cpp
+++ 230904_вложенный класс/Primer.cpp
@@ -1,3 +1,5 @@
+// пример множественного наследования
+
 #include <iostream>
 
 using namespace std;
@@ -77,10 +79,114 @@ public:
 	}
 };
 
+template<class T>
+class Couple
+{
+protected:
+	T a;
+	T b;
+public:
+	Couple(T t1, T t2) : a{ t1 }, b{ t2 } {};
+	friend ostream& operator << (ostream& out, Couple c)
+	{
+		out << c.a << " " << c.b;
+		return out;
+	
+	}
+
+
+};
+
+template<class T>
+class Trio : Couple<T>
+{
+protected:
+	T c;
+public:
+	Trio(T t1, T t2, T t3) : Couple<>
+
+		// ... код недописан ...
+
+};
+
+
+
 int main()
 {
 	Truck truck(18, 300, "Furniture", 5000);
 	truck.LoadMore(2500);
 	truck.AccellerateBy(100);
 	truck.Display();
+
+	Trio<string> t{ "Banana", "Apple", "Orange" };
+	cout << "Trio:" << t << endl;
+	return 0;
 }
+
+// Создайте класс Квадрат, содержащий верхний левый угол и сторону
+// Создайте класс Круг, содержащий центр и радиус
+// Создайте класс Круг, вписанный в квадрат, унаследованный от классов выше
+// 
+
+
+class Point
+{
+	int x;
+	int y;
+public:
+	Point() : x{ 0 }, y{ 0 }{}
+	Point(int xp, int yp) :x{ xp }, y{ yp }{};
+	void SetPoint(int xp, int yp)
+	{
+		x = xp; y = yp;
+	}
+	friend ostream& operator<< (ostream& out, Point p)
+	{
+		out << "(" << p.x << " " << p.y << ")";
+		return out;
+	}
+};
+
+class Square
+{
+protected:
+	int sideLength;
+	//агрегация
+	Point* corner;
+public:
+	Square() : sideLength{ 0 }, corner{ nullptr }{}
+	
+	//void CreateSquare(int angles, int r = 255, int g = 255, int b = 255)
+	//{
+	//	angles_count = angles;
+	//	delete[] color;
+	//	color = new int[] {r, g, b};
+	//	corner = new Point[angles_count];
+	//	if (!corner) exit(0);
+
+	//	int tempX, tempY;
+	//	for (int i = 0; i < angles_count; i++)
+	//	{
+	//		cout << "Enter X Y:" << endl;
+	//		cin >> tempX >> tempY;
+	//		corner[i].SetPoint(tempX, tempY);
+	//	}
+	//}
+
+
+};
+
+class Circle
+{
+protected:
+	int radius;
+	//агрегация
+	Point* centr;
+
+public:
+	Circle() : radius{ 0 }, centr{ nullptr }{}
+
+
+};
+
+class CircleSquare: Square, Circle
