b85e047e95b5c781e540343a016ae9a18020821b
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-09-06 21:40:58 +0500

    Add files via upload

---
+++ 230906_виртуальные функции/Source5.cpp
@@ -0,0 +1,65 @@
+// создайте абстрактный базовый класс Employee
+// У него должна быть быть чисто виртуальная функция Print()
+// Создайте три производных класса:
+// Manager, Designer, Programmer
+// Переопределите Print() для вывода соответствующей должности информации
+
+#include <iostream>
+using namespace std;
+
+class Employee
+{
+public:
+	virtual void Print() = 0;
+};
+
+class Manager :Employee
+{
+public:
+	virtual void Print();
+};
+void Manager::Print()
+{
+	cout << "This is Manager!" << endl;
+}
+
+class Designer :Employee
+{
+public:
+	virtual void Print();
+};
+void Designer::Print()
+{
+	cout << "This is Designer!" << endl;
+}
+
+class Programmer :Employee
+{
+public:
+	virtual void Print();
+};
+void Programmer::Print()
+{
+	cout << "This is Programmer!" << endl;
+}
+
+int main()
+{
+	//Employee* employes[3];
+
+	//for (int i = 0; i < 3; i++)
+	//	employes[i]->Print();
+
+	Manager manager;
+	manager.Print();
+
+	Designer designer;
+	designer.Print();
+
+	Programmer programmer;
+	programmer.Print();
+
+
+	return 0;
+}
+
