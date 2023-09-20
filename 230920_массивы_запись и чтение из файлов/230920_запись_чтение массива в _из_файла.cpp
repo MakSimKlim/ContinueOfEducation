30163e8aad594753948de08e39e1671876ec84cc
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-09-20 20:48:33 +0500

    Add files via upload

---
+++ 230920_массивы_запись и чтение из файлов/230920_запись_чтение массива в _из_файла.cpp
@@ -0,0 +1,122 @@
+﻿// 230920_запись_чтение массива в _из_файла
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+
+
+
+int main()
+{
+    char answer;
+    const int messageCount = 8;
+    int i, j;
+
+    enum
+    {
+        CHOICE = 3,// РАЗМЕРНОСТЬ МАССИВА, равно числу пнуктов в string msg
+        INPUT_FILENAME,// соответствуют индексам строчек в string msg
+        INPUT_DIMENSIONS,
+        INPUT_ELEMENTS,
+        FILE_ERROR
+
+
+    };
+
+
+    string msg[messageCount]
+    {
+        "1. Read data from the text file\n",
+        "2. Write data into the text file\n",
+        "3. Exit\n",
+        "\nYour choice:",
+        "\nEnter the file's name: ",
+        "Enter matrix size: ",
+        "Enter matrix elements: ",
+        "Couldnt open the file\n"
+    };
+
+    do
+    {
+        for (i = 0; i < 4; i++)
+        {
+            cout << msg[i];
+        }
+        cin >> answer;
+
+    } while (answer < '1' || answer > '3');
+
+    if (answer == '3')
+    {
+        return 0;
+    }
+
+    string fileName;//строка с именем файла
+    int M, N;//размеры матрицы
+    int num;
+
+    cout << msg[INPUT_FILENAME];
+    cin >> fileName;
+
+    if (answer == 1)
+    {
+        ifstream inF(fileName.c_str(), ios::in | ios::_Nocreate);//_Nocreate - файл не должен создаваться
+        if (!inF)
+        {
+            cout << endl << msg[FILE_ERROR];
+            return 0;
+
+        }
+        inF >> N;
+        inF >> M;
+        for ( i = 0; i < N; i++)
+        {
+            for (j = 0; j < M; j++)
+            {
+                inF >> num;
+                cout << setw(6) << num;
+
+
+            }
+            cout << endl;
+        }
+        inF.close();
+    }
+    else//answer ==2
+    {
+        ofstream outF(fileName.c_str(), ios::out);
+        if (!outF)
+        {
+            cout << endl << msg[FILE_ERROR];
+            return 0;
+        }
+
+        cout << msg[INPUT_DIMENSIONS];
+        cout << "\nN:"; cin >> N;
+        cout << "\nM:"; cin >> M;
+        outF << N << ' ' << M << '\n';
+        cout << msg[INPUT_ELEMENTS];
+        for ( i = 0; i < N; i++)
+        {
+            for ( j = 0; j < M; j++)
+            {
+                cout << "A[" << i << ", " << j << "] = ";
+                cin >> num;
+                outF << num << ' ';
+
+            }
+            outF << '\n';
+
+        }
+        outF.close();
+    }
+
+
+    return 0;
+}
+
---
+++ 230920_массивы_запись и чтение из файлов/230920_массивы из файла через классы.cpp
@@ -0,0 +1,139 @@
+﻿// 230920_массивы из файла через классы
+//
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int Menu()
+{
+	cout << "\n1. Add new object to file\n";
+	cout << "2. Show all objects in file\n";
+	cout << "3. Exit\n";
+	cout << "> ";
+	int choice;
+	cin >> choice;
+	return choice;
+}
+
+
+class Person
+{
+	int age;
+	string name;
+	string surname;
+public:
+	Person(string, string, int);
+	Person();
+	~Person();
+
+	void Put();
+	void Show();
+	void SaveToFile();
+	static void DisplayFromFile();//статик - то что доступно всему классу
+
+};
+
+Person::Person()
+{
+	name = "";
+	surname = "";
+	age = 0;
+}
+
+Person::Person(string nameP, string surnameP, int ageP):
+	name{ nameP }, surname{ surnameP }, age{ageP}
+{}
+
+Person::~Person()
+{}
+
+void Person::Put()
+{
+	cout << "\nEnter name: ";
+	cin >> name;
+	cout << "\nEnter surname: ";
+	cin >> surname;
+	cout << "\nEnter age: ";
+	cin >> age;
+}
+
+void Person::Show()
+{
+	cout << name << " " << surname << ", aged " << age << " years.\n";
+}
+
+void Person::SaveToFile()
+{
+	int size;
+	fstream f("people.txt", ios::out | ios::binary || ios::app);
+	if (!f)
+	{
+		cout << "Couldn t open file.";
+		exit(1);
+	}
+	// char - потому что единственный, равный 1 байт
+	// записываем символы, а потом формируем новый string из этих символов
+	f.write((char*)&age, sizeof(age));// (char*)&age - конвертация в указатель на байт с которого начинается age и потом говорим сколько считать байт sizeof(age)
+	
+	size = name.length();
+	f.write((char*)&size, sizeof(size));
+	f.write(name.c_str(), size * sizeof(char));
+
+	size = surname.length();
+	f.write((char*)&size, sizeof(size));
+	f.write(surname.c_str(), size * sizeof(char));
+
+	f.close();
+}
+void Person::DisplayFromFile()
+{
+	fstream f("people.txt", ios::in | ios::binary);
+	if (!f)
+	{
+		cout << "Couldn t open the file";
+		exit(1);
+	}
+
+	char* n, * s;
+	int a, temp;
+
+	while (f.read((char*)&a, sizeof(int)))
+	{
+		f.read((char*)&temp, sizeof(int));
+		n = new char[temp + 1];
+		f.read((char*)n, temp * sizeof(char));
+		n[temp] = '\0'; // чтобы строка выводилась корректно, добавляется символ конца строки
+
+		f.read((char*)&temp, sizeof(int));
+		s = new char[temp + 1];
+		f.read((char*)s, temp * sizeof(char));
+		s[temp] = '\0'; // чтобы строка выводилась корректно, добавляется символ конца строки
+
+		Person person{ n, s, a };//считываем данные с помощью которых создаем новые объекты
+		person.Show();
+	}
+}
+
+int main()
+{
+	int answer;
+	do
+	{
+		answer = Menu();
+		if (answer == 1)
+		{
+			Person person;
+			person.Put();
+			person.SaveToFile();
+		}
+		else
+		{
+			Person::DisplayFromFile();
+		}
+		
+	} while (answer != 3);
+
+	return 0;
+};
+
